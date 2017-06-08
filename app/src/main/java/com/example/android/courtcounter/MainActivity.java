8f4b2c0b645d44851153132da815a63c486b6a81
Jefferson Capovilla <jeffecap@motorola.com>
2017-06-08 18:16:22 -0300

    Initial commit

---
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.android.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Score 3 points.
+     */
+    public void addThreeForTeamA(View view) {
+        displayForTeamA(3);
+    }
+
+    /**
+     * Score 2 points.
+     */
+    public void addTwoForTeamA(View view) {
+        displayForTeamA(2);
+    }
+
+    /**
+     * Score 1 points.
+     */
+    public void addOneForTeamA(View view) {
+        displayForTeamA(1);
+    }
+
+}
